87dae184f48d37203b96f5fd9409789ae3d7a62c
Sh1r0w <75737051+Sh1r0w@users.noreply.github.com>
2023-08-09 17:31:29 +0200

    update user profile back end and front #9

--- src/Controllers/userSend.php
+++ src/Controllers/userSend.php
@@ -11,35 +11,37 @@ class userSend
     protected $img = null;
     protected $id = null;
 
-    public function __construct(array $input){
+    public function __construct(array $input)
+    {
         var_dump($_FILES['picture']);
         $firstName = $input['firstname'];;
         $lastName = $input['lastname'];
         $citation = $input['citation'];
         $img = $_FILES['picture']['name'];
-        $id = $_SESSION['id'];
+        $id = $_SESSION['idCo'];
         echo $id;
-        if(isset($_FILES['picture']) && $_FILES['picture']['error'] == 0 && $_FILES['picture']['size'] <= 1000000)
-        {
-            $fileInfo = pathinfo($_FILES['picture']['name']);
-            $extension = $fileInfo['extension'];
-            $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-            if(in_array($extension, $allowedExtensions))
-            {
-                move_uploaded_file($_FILES['picture']['tmp_name'], '../uploads/' . basename($_FILES['picture']['name']));
-                echo 'test image';
-                //var_dump($input);
-            }
-
+        if (isset($id)) {
             $statement = \Controllers\Fonction\db::connectDatabase()->prepare(
                 "INSERT INTO ae_user(firstname, lastname,pictures, citation, id_login) VALUES(?,?,?,?,?)"
             );
-            $sendUser = $statement->execute([$firstName, $lastName, $img, $citation, $id]);
-            return ($sendUser > 0);
+            if (!empty($img)) {
+                echo $img;
+                $fileInfo = pathinfo($_FILES['picture']['name']);
+                $extension = $fileInfo['extension'];
+                $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+                if (in_array($extension, $allowedExtensions)) {
+                    move_uploaded_file($_FILES['picture']['tmp_name'], '../uploads/' . basename($_FILES['picture']['name']));
+                }
+                $sendUser = $statement->execute([$firstName, $lastName, $img, $citation, $id]);
+                return ($sendUser > 0);
+                header('location: /user');
+            } else {
+                $sendUser = $statement->execute([$firstName, $lastName, null, $citation, $id]);
+                return ($sendUser > 0);
+                header('location: /user');
+            }
         } else {
             echo 'nop';
         }
-
-
     }
 }
