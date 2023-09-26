329efa5eed3fb85b01f5e3a0cad1a1e5f184fc10
Sh1r0w <k4leth@gmail.com>
2023-09-26 09:30:04 +0200

    Update Front and CSP file

--- src/Controllers/Fonction/csp.php
+++ src/Controllers/Fonction/csp.php
@@ -5,6 +5,6 @@ namespace Controllers\Fonction;
 class csp {
     public function __construct()
     {
-        header("Content-Security-Policy: default-src 'self' ; style-src 'self' 'unsafe-inline' cdn.jsdelivr.net; script-src 'self' cdn.jsdelivr.net");
+        header("Content-Security-Policy: default-src 'self' ; style-src 'self' 'unsafe-inline' cdn.jsdelivr.net; script-src 'unsafe-inline' 'self' cdn.jsdelivr.net");
     }
 }
